26aa56da9763022a2b21d140a3fdcd6e1385a1ef
Mohit Soni <developer_mohitsoni@outlook.com>
2024-03-30 10:33:03 +0530

    Configuration for Production Deployment

--- Project-1/vite.config.js
+++ Project-1/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "./build/app/", // Specify the output directory for the build process, defaults to 'dist'
+    sourcemap: true,
+  },
+});
